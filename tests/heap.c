bcbd16e9a43d3f223d84a1e31536be8a7df7ac08
joe <no>
2023-06-09 22:20:40 +0100

    tests: add some kfree() error tests

--- tests/heap.c
+++ tests/heap.c
@@ -1,5 +1,8 @@
 #include <rotary/tests/heap.h>
 
+extern void * heap_start_vaddr;
+extern void * heap_end_vaddr;
+
 uint32 test_heap() {
 
     printk(LOG_DEBUG, "STARTING KERNEL HEAP TEST..\n");
@@ -13,16 +16,26 @@ int32 test_kmalloc() {
 
     // Invalid Size: 0
     void * retval = kmalloc(0);
-    if(retval != (void*)VMM_ERR_INVALID_SIZE) {
+    if((uint32)retval >= 1) {
         printk(LOG_DEBUG, "FAIL: kmalloc() did not return an error when size=0\n");
         return TEST_FAILURE;
+    } else {
+        printk(LOG_DEBUG, "PASS: kmalloc() errors when size=0\n");
     }
 
-    // Invalid Size: Negative
-    retval = kmalloc(-1);
-    if(retval != (void*)VMM_ERR_INVALID_SIZE) {
-        printk(LOG_DEBUG, "FAIL: kmalloc() did not return an error when size is negative (returned %d)\n");
+    void * kfree_addr = heap_start_vaddr - 256;
+    uint32 kfree_retval = kfree(kfree_addr);
+    if(kfree_retval >= 1) {
+        printk(LOG_DEBUG, "FAIL: kfree() did not error when ptr (0x%x) out of heap bounds (0x%x -> 0x%x)\n",
+                kfree_addr,
+                heap_start_vaddr,
+                heap_end_vaddr);
         return TEST_FAILURE;
+    } else {
+        printk(LOG_DEBUG, "PASS: kfree() errors when ptr (0x%x) out of heap bounds (0x%x -> 0x%x)\n",
+                kfree_addr,
+                heap_start_vaddr,
+                heap_end_vaddr);
     }
 
     printk(LOG_DEBUG, "All kmalloc() tests passed!\n");
