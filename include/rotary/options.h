168504ea5a67a8ce10cdb6f494c3c5656512d187
joe <joestanding@users.noreply.github.com>
2023-06-12 14:34:24 +0000

    options: add kernel base define

--- include/rotary/options.h
+++ include/rotary/options.h
@@ -6,5 +6,6 @@
 #define KERNEL_NAME             "Rotary"
 #define KERNEL_VERSION          "0.1"
 #define KERNEL_MEMZERO_ON_FREE  1
+#define KERNEL_BASE             0xC0000000
 
 /* ========================================================================= */
